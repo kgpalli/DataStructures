361faf4de4d8c1336efca5033cc8ccfd064915d0
Kalpana <kalpana.gollapalli02@gmail.com>
2021-06-17 23:05:43 -0700

    Pre Order

---
+++ src/test/java/dst/dsLinkedList/PreOrder.java
@@ -0,0 +1,30 @@
+package dst.BST;
+/**
+ * 589. N-ary Tree Preorder Traversal
+ * Input: root = [1,null,3,2,4,null,5,6]
+ * Output: [1,3,5,6,2,4]
+ *
+ * */
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PreOrder {
+
+    public List<Integer> preorder(Node root) {
+        LinkedList<Integer>output_arr = new LinkedList<>();
+
+
+
+     return output_arr;
+    }
+
+    public static void main(String[]args){
+        TreeNode node = new TreeNode();
+
+       node.root = new Node(1);
+
+
+    }
+
+}
