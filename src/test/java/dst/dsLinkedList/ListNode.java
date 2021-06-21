7aa34cd8098e574d3c3c09b54d23cf819f0f8d31
Kalpana <kalpana.gollapalli02@gmail.com>
2021-06-20 21:19:42 -0700

    POst Order

--- src/test/java/dst/dsLinkedList/ListNode.java
+++ src/test/java/dst/dsLinkedList/ListNode.java
@@ -1,11 +1,15 @@
 package dst.dsLinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListNode {
     int val;
     ListNode next;
+    List<ListNode> children;
     ListNode(int val){
         this.val = val;
         this.next= null;
+        this.children = new ArrayList<>();
     }
 
 
--- src/test/java/dst/dsLinkedList/Postorder.java
+++ src/test/java/dst/dsLinkedList/Postorder.java
@@ -1,19 +1,16 @@
 package dst.dsLinkedList;
-/**
- * 589. N-ary Tree Preorder Traversal
- * Input: root = [1,null,3,2,4,null,5,6]
- * Output: [1,3,5,6,2,4]
- *
- * */
+
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
-public class PreOrder {
+public class Postorder {
 
-    public static List<Integer> preorder(ListNode root) {
-        LinkedList<Integer>output_arr = new LinkedList<>();
-        LinkedList<ListNode>stack = new LinkedList<>();
+    public static List<Integer> postorder(ListNode root) {
+        LinkedList<Integer> output_arr = new LinkedList<>();
+       LinkedList<ListNode> stack = new LinkedList<>();
+
         if(root==null){
             return output_arr;
         }
@@ -21,15 +18,16 @@ public class PreOrder {
         stack.add(root);
         while(!stack.isEmpty()){
             ListNode node = stack.pollLast();
-            output_arr.add(node.val);
-            Collections.reverse(node.children);
+           output_arr.addFirst(node.val);
+           System.out.println("Out put array is + " + output_arr);
             stack.addAll(node.children);
         }
+        return output_arr;
 
-     return output_arr;
     }
 
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         ListNode node = new ListNode(1);
         node.children.add(new ListNode(3));
         node.children.add(new ListNode(2));
@@ -38,12 +36,6 @@ public class PreOrder {
         node.children.get(0).children.add(new ListNode(6));
 
 
-
-        System.out.println(preorder(node));
-
-
-
-
+        System.out.println(postorder(node));
     }
-
 }
